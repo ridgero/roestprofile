ca12c26d1c1233543775714be82604d1759551c7
ridgero <119787528+ridgero@users.noreply.github.com>
2026-08-29 19:27:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Minimaler Service Worker: cacht die App-Shell, damit das Tool
+// auch ohne Netz startet (z.B. wenn die Röstküche kein WLAN hat).
+const CACHE_NAME = 'roestprofile-v7';
+const ASSETS = [
+  './roestprofile-tool-v7.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
